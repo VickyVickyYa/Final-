cd0a6d030f556b844a38d60a5f22d778a96ee39e
Victoria <Victoria.Afe1@gmail.com>
2025-10-25 06:57:19 -0400

    work

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ answers: [
 
 },
 {
-question: "Which Shakespearean play is often associated with Halloween themes, particularly witches and the supernatural? ",
+question: "Which Shakespearean play is often associated with witches and the supernatural? ",
 answers: [
     {text: "Beetle Juice", correct: false},
     {text: "Macbeth", correct: true},
@@ -43,4 +43,68 @@ answers: [
 
 
 
-]
+]; 
+
+const questionElement = document.getElementById("question"); 
+const answerButtons = document.getElementById("answer-buttons"); 
+const nextButton = document.getElementById("next-btn");
+
+
+let currentQuestionIndex = 0; 
+let score = 0; 
+
+function startQuiz (){
+currentQuestionIndex = 0;
+score = 0;
+nextButton.innerHTML= "Next";
+showQuestion();
+}
+
+function showQuestion () { 
+    resetState();
+let currentQuestion = questions[currentQuestionIndex];
+let questionNo= currentQuestionIndex + 1;
+questionElement.innerHTML = questionNo + ". " + currentQuestion.
+question;
+
+currentQuestion.answers.forEach(answer => {
+const button = document.createElement("button");
+button.innerHTML = answer.text; 
+button.classList.add("btn");
+answerButtons.appendChild
+if (answer.correct) {
+    button.dataset.correct = answer.correct;
+}
+button.addEventListener("click", selectAnswer )
+
+});
+
+} 
+
+function resetState(){
+nextButton.style.display = "none";
+while (answerButtons.firstChild) {
+    answerButtons.removeChild ( answerButtons.firstChild);
+}
+
+}
+
+function selectAnswer (e)
+{ const selectedBtn = e.target; 
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect) {
+        selectedBtn.classlist.add("correct"); 
+    }else {
+        selectedBtn.classList.add( "incorrect");
+
+    }
+    Array.from(answerButtons.children).forEach(button => {
+if ( button.dataset.correct ==="true") {
+    button.classList.add("correct");
+}
+button.disabled = true; 
+  });
+  nextButton.style.display = "block"; 
+}
+
+startQuiz ();
