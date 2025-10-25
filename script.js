1db05bbad71060e8910ac114770e782f90601d90
Victoria <Victoria.Afe1@gmail.com>
2025-10-25 01:22:01 -0400

    making the questions

---
+++ script.js
@@ -0,0 +1,46 @@
+const questions = [
+{
+question: "What vegetable was used to carve jack-o'lanters before pumpkins ?",
+answers: [
+    {text: "Apples", correct: false},
+    {text: "Turnips", correct: true},
+    {text: "Potatoes", correct: false},
+    {text: "Butternut Squash", correct: false},
+]
+
+},
+{
+
+question: "In what horror film does the villain wear a hockey mask and follows people into the woods ?",
+answers: [
+    {text: "Halloween", correct: false},
+    {text: "Scream", correct: false},
+    {text: "Fear street 1984", correct: false},
+    {text: "Friday the 13th", correct: true},
+]
+
+},
+{   
+question: "Which iconic 1978 slasher film is credited with starting the slasher genre, featuring a killer named Michael Myers? ?",
+answers: [
+    {text: "Halloween", correct: true},
+    {text: "Poltergeist", correct: false},
+    {text: "Scream Queens", correct: false},
+    {text: "Chucky", correct: false},
+]
+
+},
+{
+question: "Which Shakespearean play is often associated with Halloween themes, particularly witches and the supernatural? ",
+answers: [
+    {text: "Beetle Juice", correct: false},
+    {text: "Macbeth", correct: true},
+    {text: "Hamlet", correct: false},
+    {text: "Romeo and Julliet", correct: false},
+]
+}
+
+
+
+
+]
